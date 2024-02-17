59bd4549f5ccbe64c29de6ec70ec5c568a1ed6b1
novelkhan <novel4004@gmail.com>
2024-02-17 10:09:07 +0600

    File upload routing is added

--- views/src/app/modules/file/file-routing.module.ts
+++ views/src/app/modules/file/file-routing.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
 import { FilesComponent } from './files.component';
 import { AllFilesComponent } from './components/all-files/all-files.component';
+import { UploadFileComponent } from './components/upload-file/upload-file.component';
 
 const routes: Routes = [
   {path: '', component: FilesComponent},
-  {path: 'all-files', component: AllFilesComponent}
+  {path: 'all-files', component: AllFilesComponent},
+  {path: 'upload', title: 'Upload' ,component: UploadFileComponent}
 ];
 
 @NgModule({
